604c9309f0040dd6cd18d0d99358aab262223237
BB <bbedward@gmail.com>
2020-04-23 13:47:50 -0400

    Add message to log format

--- server/bpow/logger.py
+++ server/bpow/logger.py
@@ -5,7 +5,7 @@ from logging.handlers import WatchedFileHandler, TimedRotatingFileHandler
 def get_logger(stdout: bool = False):
     logger = logging.getLogger("bpow")
     logging.basicConfig(level=logging.INFO)
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s@%(funcName)s:%(lineno)s", "%Y-%m-%d %H:%M:%S %z")
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s@%(funcName)s:%(lineno)s:%(message)s", "%Y-%m-%d %H:%M:%S %z")
     if not stdout:
         log_file = "/tmp/bpow.log"
         handler = WatchedFileHandler(log_file)
